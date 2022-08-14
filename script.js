d336c1f466793fc7efc14e95f3759f3a951d3315
David <davidalfonsook@gmail.com>
2022-08-14 19:22:31 -0400

    falta la pantalla cuando se termina

--- script.js
+++ script.js
@@ -28,28 +28,33 @@ let computerSelection = getComputerChoice();
     return temp;
 }*/
 
+let ronda;
+
 function playRound (playerSelection) {
     
     computerSelection = getComputerChoice();
     if (playerSelection == "ROCK" && computerSelection == "SCISSORS") {
-        //console.log("You win, " + playerSelection + " beats " + computerSelection);
-        return("user");
+        //return("You win, " + playerSelection + " beats " + computerSelection);
+        return "user";
     }
     else if (playerSelection == "SCISSORS" && computerSelection == "PAPER") {
         //console.log("You win, " + playerSelection + " beats " + computerSelection);
-        return("user");
+        //return("You win, " + playerSelection + " beats " + computerSelection);
+        return "user";
     }
     else if (playerSelection == "PAPER" && computerSelection == "ROCK") {
         //console.log("You win, " + playerSelection + " beats " + computerSelection);
-        return("user");
+        //return("You win, " + playerSelection + " beats " + computerSelection);
+        return "user";
     }
     else if (playerSelection == computerSelection) {
         //console.log("Tie!");
-        return ("Tie");
+        return ("Tie!");
     }
     else {
         //console.log("You lose, " + computerSelection + " beats " + playerSelection);
-        return("computer");
+        //return("You lose, " + computerSelection + " beats " + playerSelection);
+        return "pc";
     }
 }
 
@@ -57,10 +62,29 @@ let retorno = "";
 
 //boton para piedra
 const btn1 = document.querySelector('#btn1');
+const imgUser = document.querySelector('#imgUser');
+//imagen de la maquina
+const imgPc = document.querySelector('#imgPc');
+
 
 btn1.addEventListener('click', () => {
     retorno = playRound("ROCK");
-    parrafo.textContent = retorno;
+    imgUser.setAttribute('src', 'images/rock.png');
+    
+    if (retorno == "user") {
+        imgPc.setAttribute('src', 'images/scissor.png');
+        scoreUser.textContent = parseInt(scoreUser.textContent) + 1;
+        parrafo.textContent = "You win, rock beats scissor";
+    }
+    else if (retorno == "Tie!") {
+        imgPc.setAttribute('src', 'images/rock.png');
+        parrafo.textContent = "Tie!";
+    }
+    else {
+        imgPc.setAttribute('src', 'images/paper.png');
+        scorePc.textContent = parseInt(scorePc.textContent) + 1;
+        parrafo.textContent = "Computer wins, paper beats rock";
+    }
 })
 
 //boton para papel
@@ -70,13 +94,46 @@ const btn2 = document.querySelector('#btn2');
 btn2.addEventListener('click', () => {
     retorno = playRound("PAPER");
     parrafo.textContent = retorno;
+    imgUser.setAttribute('src', 'images/paper.png');
+
+    if (retorno == "user") {
+        imgPc.setAttribute('src', 'images/rock.png');
+        scoreUser.textContent = parseInt(scoreUser.textContent) + 1;
+        parrafo.textContent = "You win, paper beats rock";
+    }
+    else if (retorno == "Tie!") {
+        imgPc.setAttribute('src', 'images/paper.png');
+        parrafo.textContent = "Tie!";
+    }
+    else {
+        imgPc.setAttribute('src', 'images/scissor.png');
+        scorePc.textContent = parseInt(scorePc.textContent) + 1;
+        parrafo.textContent = "Computer wins, scissors beats paper";
+    }
 })
 
 //boton para tijeras
+const btn3 = document.querySelector('#btn3');
 
-const btn3 = document.addEventListener('click', () => {
+btn3.addEventListener('click', () => {
     retorno = playRound("SCISSORS");
     parrafo.textContent = retorno;
+    imgUser.setAttribute('src', 'images/scissor.png');
+
+    if (retorno == "user") {
+        imgPc.setAttribute('src', 'images/paper.png');
+        scoreUser.textContent = parseInt(scoreUser.textContent) + 1;
+        parrafo.textContent = "You win, scissors beats paper";
+    }
+    else if (retorno == "Tie!") {
+        imgPc.setAttribute('src', 'images/scissor.png');
+        parrafo.textContent = "Tie!";
+    }
+    else {
+        imgPc.setAttribute('src', 'images/rock.png');
+        scorePc.textContent = parseInt(scorePc.textContent) + 1;
+        parrafo.textContent = "Computer wins, rock beats scissors";
+    }
 })
 
 //div para los resultados
@@ -88,13 +145,15 @@ results.setAttribute('id', 'results');
 
 const parrafo = document.createElement('p');
 results.appendChild(parrafo);
- 
-results.appendChild(parrafo);
+parrafo.style.fontFamily = 'DynaPuff';
 
 const cuerpo = document.querySelector('body');
 cuerpo.appendChild(results);
 
+let scoreUser = document.querySelector('#scoreUser');
+let scorePc = document.querySelector('#scorePc');
+//scoreUser.textContent = parseInt(scoreUser.textContent) + 1;
 
-
-
-
+results.style.display = 'flex';
+results.style.justifyContent = 'center';
+results.style.marginTop = '20px';
